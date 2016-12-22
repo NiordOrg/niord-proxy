42f437469e2d8a7ac969bfb0b90264147f74166e
peder280370 <peder@carolus.dk>
2016-12-22 08:16:40 +0100

    Minor bug-fix

--- src/main/webapp/app/app-directives.js
+++ src/main/webapp/app/app-directives.js
@@ -160,7 +160,7 @@ angular.module('niord.proxy.app')
                     if (scope.ref.descs && scope.ref.descs.length > 0 && scope.ref.descs[0].description) {
                         result += ' - ' + scope.ref.descs[0].description;
                     }
-                    if (result.length > 0 && result.charAt(result.length-1) != '.') {
+                    if (result.length == 0 || result.charAt(result.length-1) != '.') {
                         result += '.';
                     }
                     element.html(result);
